ae524d2f8577eb3ded9932434af4e419d4486e90
Vasanth Selvam <therealvasanth@gmail.com>
2017-09-18 21:58:26 +0530

    finishing speech customising options

--- js/app.js
+++ js/app.js
@@ -1,10 +1,47 @@
 var info = "Paste any text here and click the SPEAK button below to convert the text in to speech.";
+var nativeVoice = 1, voices, speechSpeed = 0.8;
+var setSpeed = function(x) {
+    var x = document.getElementById("select-speed").value;
+    speechSpeed = x;
+}
+var setVoice = function() {
+    voices = document.getElementById("select-voice");
+    nativeVoice = voices.options[voices.selectedIndex].value;
+    setSpeed();
+    switch(nativeVoice){
+        case "1":
+            responsiveVoice.speak(info, "UK English Female",{rate: speechSpeed});
+            break;
+        case "2":
+            responsiveVoice.speak(info, "UK English Male", {rate: speechSpeed});
+            break;
+        case "3":
+            responsiveVoice.speak(info, "US English Female", {rate: speechSpeed});
+            break;
+        case "4":            
+            responsiveVoice.speak(info, "US English Male", {rate: speechSpeed});
+            break;
+        case "5":
+            responsiveVoice.speak(info, "Australian Female", {rate: speechSpeed});
+            break;
+        case "6":
+            responsiveVoice.speak(info, "Dutch Female", {rate: speechSpeed});
+            break;
+        case "7":
+            responsiveVoice.speak(info, "Hindi Female", {rate: speechSpeed});
+            break;
+        case "8":
+            responsiveVoice.speak(info, "Japanese Female", {rate: speechSpeed});
+            break;
+        case "9":
+            responsiveVoice.speak(info, "Korean Female", {rate: speechSpeed});
+            break;
+    }
+}
+
 
 var speakHello = function(){
-    //  responsiveVoice.speak(info, "UK English Male", {volume: 1});  // volume
-    //  responsiveVoice.speak(info, "UK English Male", {rate: 1.5});  // speed
-    //  responsiveVoice.speak(info, "UK English Male", {pitch: 0.9}); //pitch
-    // 	responsiveVoice.speak(info);        
+        setVoice();      
 };
 
 var stopSpeech = function() {
